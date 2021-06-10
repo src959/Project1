9f7bcde59405eeb29297cdffc79628efd616e923
src959 <82751657+src959@users.noreply.github.com>
2021-06-10 17:53:20 +0530

    added.

--- app/views.py
+++ app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def sandeep(request):
-    s="<h1> Hello This is first project and this is nursery level </h1>"
+    s="<h1> Hello This is first project and this is nursery level.. </h1>"
     return HttpResponse(s)
